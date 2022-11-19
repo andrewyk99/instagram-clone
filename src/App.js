5ccad029b44251666ec63c3a312e78af57930903
andrewyk99 <andrewyk99@hotmail.com>
2022-11-18 22:29:13 -0700

    setup tailwind

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
+
 const Login = lazy(() => import ('./pages/login'));
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import FirebaseContext from './context/firebase';
 import { firebase, FieldValue } from './lib/firebase';
+import './styles/app.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <FirebaseContext.Provider value={{firebase, FieldValue }}>
-        <App />
-    </FirebaseContext.Provider>
+ReactDOM.render(
+    <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+      <App />
+    </FirebaseContext.Provider>,
+    document.getElementById('root')
 );
 
 // Client side rendered app: react (cra)
